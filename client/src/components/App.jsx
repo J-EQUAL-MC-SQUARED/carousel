e47d50f57d5bb2496bf76537fb6f066ec153031a
Justin Chow <hrll71990@gmail.com>
2021-01-15 12:02:53 -0800

    create App, Carousel components: App GETs (random) related items on mount, Carousel will eventually handle passing item data to other components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      id: Math.floor(Math.random() * 100),
+      items: [],
+    };
+    this.getRelatedItems = this.getRelatedItems.bind(this);
+  }
+
+  componentDidMount() {
+    this.getRelatedItems();
+  }
+
+  getRelatedItems() {
+    const { id } = this.state;
+    axios.get(`/api/products/${id}/images`)
+      .then((response) => {
+        const { relatedItems } = response.data[0];
+        this.setState({ items: relatedItems });
+      });
   }
 
   render() {
     return (
       <div>
-        Hello World
+        <h2>List of Similar Items:</h2>
       </div>
     );
   }
---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="carousel-main">
+        something
+      </div>
+    );
+  }
+}
+
+export default Carousel;
