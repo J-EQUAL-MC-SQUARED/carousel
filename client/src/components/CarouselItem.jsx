2423407cfcb392f7c9a885520b2588001de97685
Justin Chow <hrll71990@gmail.com>
2021-01-15 14:14:35 -0800

    displaying raw data on page, but need to: resize images, align correctly items, add unique classnames for each HTML element

--- client/src/components/CarouselItem.jsx
+++ client/src/components/CarouselItem.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 const CarouselItem = (props) => {
   return (
-    <div>{JSON.stringify(props)}</div>
+    <div>
+      <img src={props.imageUrl}></img>
+      <div className="item-body">
+        <div>
+          {props.name}
+        </div>
+        <div>
+          {`Stars: ${props.stars}`}
+        </div>
+        <div>
+          {`Price: ${props.price}`}
+        </div>
+      </div>
+    </div>
   )
 }
 
