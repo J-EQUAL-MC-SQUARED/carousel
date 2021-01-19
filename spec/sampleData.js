9081c795fbbc39f0544f767afe8e4dc5cab339ae
Justin Chow <hrll71990@gmail.com>
2021-01-18 23:48:21 -0800

    beginning testing: testing dependencies installed, CarouselItem and CarouselList tests written, passing

---
+++ spec/sampleData.js
@@ -0,0 +1,74 @@
+const sampleRelatedItems = [
+  {
+      "imageUrl": "https://picsum.photos/650/460",
+      "name": "Indoor Leather Bathtub",
+      "stars": 3.3,
+      "description": "Consequat duis et officia labore consequat. Laboris in eu excepteur minim ut magna consectetur sint laboris.",
+      "price": 304.11
+  },
+  {
+      "imageUrl": "https://picsum.photos/650/460",
+      "name": "Recreational Wooden Wine Rack",
+      "stars": 4.3,
+      "description": "Amet est amet veniam do amet qui. Sit labore eu ullamco adipisicing est eu reprehenderit anim. Nulla ut aliquip amet laboris esse. Adipisicing esse nostrud quis magna est dolore ut.",
+      "price": 85.11
+  },
+  {
+      "imageUrl": "https://picsum.photos/650/460",
+      "name": "Recreational Beige Beer",
+      "stars": 3.1,
+      "description": "Est excepteur anim reprehenderit ipsum culpa nostrud esse officia ex. Qui adipisicing et ex elit ex aliquip incididunt. Eiusmod elit qui commodo non est consectetur.",
+      "price": 209.13
+  },
+  {
+      "imageUrl": "https://picsum.photos/650/460",
+      "name": "Kid's Oak Patio Set",
+      "stars": 0.5,
+      "description": "Ad ea cupidatat cupidatat labore do ullamco minim. Dolor elit minim velit officia commodo proident mollit do Lorem. Nostrud reprehenderit ullamco exercitation voluptate laboris sint sit Lorem et.",
+      "price": 472.71
+  },
+  {
+      "imageUrl": "https://picsum.photos/650/460",
+      "name": "Kid's White Lamp",
+      "stars": 3.1,
+      "description": "Est enim duis mollit esse laboris non labore minim amet. Velit sunt consequat mollit nostrud. Dolore est voluptate veniam incididunt do dolor pariatur. Ut labore velit pariatur amet non esse commodo elit.",
+      "price": 337.76
+  },
+  {
+      "imageUrl": "https://picsum.photos/650/460",
+      "name": "Kid's White Chair Set",
+      "stars": 1.4,
+      "description": "Ut veniam ad qui id excepteur. Minim elit culpa tempor officia.",
+      "price": 16.29
+  },
+  {
+      "imageUrl": "https://picsum.photos/650/460",
+      "name": "Sharp Leather Lamp",
+      "stars": 3,
+      "description": "Adipisicing officia fugiat proident anim. Consectetur eu duis nostrud officia aute officia aute duis. Et duis pariatur nulla laboris.",
+      "price": 248.56
+  },
+  {
+      "imageUrl": "https://picsum.photos/650/460",
+      "name": "Kid's Non-toxic Lamp",
+      "stars": 2.7,
+      "description": "Elit ipsum eu pariatur proident anim laborum occaecat dolore duis. Minim esse exercitation anim proident do incididunt tempor excepteur. Sunt consectetur elit irure exercitation do fugiat tempor quis ullamco. Do non do do deserunt ut ullamco veniam.",
+      "price": 469.38
+  },
+  {
+      "imageUrl": "https://picsum.photos/650/460",
+      "name": "Sharp Ceramic Kitchen Knife",
+      "stars": 3.7,
+      "description": "Exercitation in tempor est id sint laboris laboris tempor. Sint ea sint officia veniam consequat eu velit cillum.",
+      "price": 383.19
+  },
+  {
+      "imageUrl": "https://picsum.photos/650/460",
+      "name": "Home Wooden Kitchen Knife",
+      "stars": 4.9,
+      "description": "Veniam amet et aute deserunt ex anim. Irure elit consectetur sunt ea Lorem deserunt veniam elit eu. Eu non incididunt culpa velit sint.",
+      "price": 483.22
+  },
+]
+
+export default sampleRelatedItems;
--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,6 @@ module.exports = {
         use: [
           {
             loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-            },
           },
         ],
       },
