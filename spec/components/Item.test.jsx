0707b79a687290fb4ba428c84a6d8adcfc9387fd
Justin Chow <hrll71990@gmail.com>
2021-01-19 21:59:08 -0800

    Item component test refactored: tried to use more Jest Queries, assert by what user sees on DOM, abstract away from HTML implementation details

--- spec/components/Item.test.jsx
+++ spec/components/Item.test.jsx
@@ -4,29 +4,26 @@ import { render, screen } from '@testing-library/react';
 import Item from '../../client/src/components/Item';
 import sampleRelatedItems from '../sampleData';
 
+const { imageUrl, name, stars, price } = sampleRelatedItems[Math.floor(Math.random() * 9)];
+render(<Item
+    imageUrl={imageUrl}
+    name={name}
+    stars={stars}
+    price={price}
+  />);
+
 describe('<Item />', () => {
-  beforeEach(() => {
-    const { imageUrl, name, stars, price } = sampleRelatedItems[Math.floor(Math.random() * 9)];
-    render(<Item
-      imageUrl={imageUrl}
-      name={name}
-      stars={stars}
-      price={price}
-      />);
-  });
-  
   test('should render a product\'s image, name, star rating, and price', () => {
     const image = screen.getByRole('img');
     expect(image).toBeInTheDocument();
-    expect(image).toHaveAttribute('src');
-    expect(image.getAttribute('src')).not.toBeUndefined();
+    
+    const name = document.querySelector('.item-body-name');
+    expect(name).toBeInTheDocument();
+
+    const stars = document.querySelector('#stars-container');
+    expect(stars).toBeVisible();
+
+    const price = screen.getByText((content) => content.startsWith('Today: $'));
+    expect(price).toBeInTheDocument();
   });
-
-  // test('should render a name', () => {
-  //   const name = document.querySelector('.item-body-name');
-  //   expect(name).toBeInTheDocument();
-  //   expect(name).not.toBeEmptyDOMElement();
-  // });
-
-  // TODO: tests for stars, price
 });
