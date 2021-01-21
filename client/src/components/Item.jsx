ea94e7a63a96296b767e2d28b5b65d5a2acbf90a
Justin Chow <hrll71990@gmail.com>
2021-01-21 12:30:55 -0800

    modal now appears on hovering over a carousel item; need to add more styling for appearance

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Stars from './Stars';
 import Modal from './Modal';
 
 const Item = ({
   imageUrl, name, stars, price, description,
-}) => {
-  const [modal, setModal] = useState(false);
+}) => (
 
-  return (
-    <div className="carousel-list-item">
-      <img className="item-img" src={imageUrl} alt="carousel-item" />
-      <div className="item-body">
-        <div className="item-body-name">
-          {name}
-        </div>
-        <div className="item-body-stars">
-          <Stars stars={stars} />
-        </div>
-        <div className="item-body-price">
-          {`Today: $${price}`}
-        </div>
+  <div className="carousel-list-item">
+    <img className="item-img" src={imageUrl} alt="carousel-item" />
+    <div className="item-body">
+      <div className="item-body-name">
+        {name}
       </div>
-      <Modal description={description} />
+      <div className="item-body-stars">
+        <Stars stars={stars} />
+      </div>
+      <div className="item-body-price">
+        {`Today: $${price}`}
+      </div>
     </div>
-  );
-};
+    <Modal description={description} />
+  </div>
+);
 
 Item.propTypes = {
   imageUrl: PropTypes.string.isRequired,
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Modal = ({ description }) => (
-  <div className="modal">
+  <div className="carousel-modal">
     {`Modal test ${description}`}
   </div>
 );
+
+Modal.propTypes = {
+  description: PropTypes.string.isRequired,
+};
 
 export default Modal;
