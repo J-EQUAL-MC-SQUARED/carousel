2b4cc8c9927abedb1e336dd110cac08017883759
Justin Chow <hrll71990@gmail.com>
2021-01-21 11:25:58 -0800

    created Modal component, using css to adjust visibility on hover

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Stars from './Stars';
+import Modal from './Modal';
 
 const Item = ({
-  imageUrl, name, stars, price,
-}) => (
+  imageUrl, name, stars, price, description,
+}) => {
+  const [modal, setModal] = useState(false);
 
-  <div className="carousel-list-item">
-    <img className="item-img" src={imageUrl} alt="carousel-item" />
-    <div className="item-body">
-      <div className="item-body-name">
-        {name}
+  return (
+    <div className="carousel-list-item">
+      <img className="item-img" src={imageUrl} alt="carousel-item" />
+      <div className="item-body">
+        <div className="item-body-name">
+          {name}
+        </div>
+        <div className="item-body-stars">
+          <Stars stars={stars} />
+        </div>
+        <div className="item-body-price">
+          {`Today: $${price}`}
+        </div>
       </div>
-      <div className="item-body-stars">
-        <Stars stars={stars} />
-      </div>
-      <div className="item-body-price">
-        {`Today: $${price}`}
-      </div>
+      <Modal description={description} />
     </div>
-  </div>
-);
+  );
+};
 
-// Disable propTypes to clear log for testing
 Item.propTypes = {
   imageUrl: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   stars: PropTypes.number.isRequired,
   price: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
 };
 
 export default Item;
---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Modal = ({ description }) => (
+  <div className="modal">
+    {`Modal test ${description}`}
+  </div>
+);
+
+export default Modal;
