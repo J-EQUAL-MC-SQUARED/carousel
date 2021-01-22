280dd909d4c8b5f1ba5426b2d364aeae150bfba2
Justin Chow <hrll71990@gmail.com>
2021-01-21 20:52:05 -0800

    description Modal done: SEE NOTE ABOUT MODAL TESTS in Item.test.jsx

--- spec/components/Item.test.jsx
+++ spec/components/Item.test.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Item from '../../client/src/components/Item';
 import sampleRelatedItems from '../sampleData';
 
-const { imageUrl, name, stars, price } = sampleRelatedItems[Math.floor(Math.random() * 9)];
-render(<Item
-    imageUrl={imageUrl}
-    name={name}
-    stars={stars}
-    price={price}
-  />);
-
 describe('<Item />', () => {
+  beforeEach(() => {
+    const { imageUrl, name, stars, price, description } = sampleRelatedItems[Math.floor(Math.random() * 9)];
+    render(<Item
+      imageUrl={imageUrl}
+      name={name}
+      stars={stars}
+      price={price}
+      description={description}
+    />);
+  });
+
   test('should render a product\'s image, name, star rating, and price', () => {
     const image = screen.getByRole('img');
     expect(image).toBeInTheDocument();
@@ -25,5 +28,16 @@ describe('<Item />', () => {
 
     const price = screen.getByText((content) => content.startsWith('Today: $'));
     expect(price).toBeInTheDocument();
+
+    /* 
+     * NOTE: due to the way Jest or testing-library creates a 'mock' DOM, it cannot import carouselStyles.css,
+     * thus the styles defined for .carousel-modal and .carousel-item-image,
+     * which handle the modal's main behavior, are not applied.
+     * Therefore, this breaks the hover-and-appear functionality of the Modal component,
+     * and leaves no straightforward way to test that functionality here.
+    */
+    const modal = document.querySelector('.carousel-modal');
+    expect(modal).toBeVisible();
   });
+
 });
