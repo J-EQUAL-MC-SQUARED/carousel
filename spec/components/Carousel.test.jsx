318fb4f9e1f01cf73d84ed0fc375aa9eacd65647
Justin Chow <hrll71990@gmail.com>
2021-01-22 14:16:43 -0800

    for now, all tests are less brittle

--- spec/components/Carousel.test.jsx
+++ spec/components/Carousel.test.jsx
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react';
 import Carousel from '../../client/src/components/Carousel';
 
 describe('<Carousel />', () => {
-  test('should render a carousel to the page', () => {
+  test('should render a carousel to the page', async () => {
     render(<Carousel />);
     expect(screen.getByRole('main')).toBeInTheDocument();
     expect(screen.getByRole('heading')).not.toBeEmptyDOMElement();
