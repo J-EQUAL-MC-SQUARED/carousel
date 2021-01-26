b7f1af2c00284fd365f48ecdda48327fabc12e67
Justin Chow <hrll71990@gmail.com>
2021-01-26 15:57:56 -0800

    using global variable for productId - works for now

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from './components/Carousel';
 
-ReactDOM.render(<Carousel productId={99} />, document.getElementById('carousels-section'));
+const ID = window.PRODUCT_ID < 0 || 99;
+
+ReactDOM.render(<Carousel productId={ID} />, document.getElementById('carousels-section'));
