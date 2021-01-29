d2e3bf8aef1c5d4b7eac8fc0d56ede7672b967f7
Justin Chow <hrll71990@gmail.com>
2021-01-29 11:52:43 -0800

    optimizing performance speeds by loading brotli and gzip compressed webpack bundled js

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,36 @@ const path = require('path');
 const ENTRY_PATH = path.resolve(__dirname, 'client', 'src', 'index.jsx');
 const OUTPUT_PATH = path.resolve(__dirname, 'public');
 
+const CompressionPlugin = require('compression-webpack-plugin');
+const zlib = require('zlib');
+
 module.exports = {
   entry: ENTRY_PATH,
   output: {
     path: OUTPUT_PATH,
     filename: 'carouselApp.js',
   },
+  plugins: [
+    new CompressionPlugin({
+      filename: '[path][base].gz',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: '[path][base].br',
+      algorithm: 'brotliCompress',
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
   module: {
     rules: [
       {
